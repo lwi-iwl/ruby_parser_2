5f0d75e4e8eb07c66481fa8fd3ffe594076722b9
lwi-wil <nikstrach@gmail.com>
2022-02-05 18:44:56 +0300

    constants

--- blueskymss_parser.rb
+++ blueskymss_parser.rb
@@ -3,6 +3,14 @@ require 'json'
 require 'ox'
 
 class BlueskymssParser
+  TOKEN_QUERY = 'client_id=247medstaff&application_key=9E9E914F397943A8989763538EF67025&grant_type=password'
+  TOKEN_URL = 'https://bssservice.blueskymss.com/auth/token'
+  LIST_URL = "https://api.blueskymss.com/api/Needs/list"
+  DETAILS_URL = "https://api.blueskymss.com/api/Needs/jobDetails"
+  SITE_URL = "https://jobboard.blueskymss.com/JobBoard/jobBoard.html?a=247medstaff&it=3&s=1&bss=2&c=1&fv=111&fc=1#"
+  DATE_FROM = '01/05/2022'
+  DATE_TO = '01/30/2023'
+
   def generate_xml(path)
     doc = Ox::Document.new(:version => '1.0')
     jobs = get_jobs
@@ -20,23 +28,21 @@ class BlueskymssParser
 
   def get_jobs
     jobs = Ox::Element.new('jobs')
-    date_from = '01/05/2022'
-    date_to = '01/30/2023'
     @mechanize = Mechanize.new
-    token_query = 'client_id=247medstaff&application_key=9E9E914F397943A8989763538EF67025&grant_type=password'
-    info = @mechanize.post('https://bssservice.blueskymss.com/auth/token',
-                           token_query, {
+
+    info = @mechanize.post(TOKEN_URL,
+                           TOKEN_QUERY, {
                              'Content-Type' => 'application/json; charset=UTF-8'
                            })
     token = JSON.parse(info.content)["access_token"]
-    info = @mechanize.post("https://api.blueskymss.com/api/Needs/list",
-                           generate_query(0, date_from, date_to), {
+    info = @mechanize.post(LIST_URL,
+                           generate_query(0, DATE_FROM, DATE_TO), {
                              'Content-Type' => 'application/json; charset=UTF-8',
                              "authorization" => "Bearer " + token
                            })
     count = JSON.parse(info.content)["count"]
-    info = @mechanize.post("https://api.blueskymss.com/api/Needs/list",
-                           generate_query(count, date_from, date_to), {
+    info = @mechanize.post(LIST_URL,
+                           generate_query(count, DATE_FROM, DATE_TO), {
                              'Content-Type' => 'application/json; charset=UTF-8',
                              "authorization" => "Bearer " + token
                            })
@@ -55,14 +61,14 @@ class BlueskymssParser
         'key':'type',
         'value':'#{parameters_row["Type"]}'
     }]"
-    info = @mechanize.post("https://api.blueskymss.com/api/Needs/jobDetails",
+    info = @mechanize.post(DETAILS_URL,
                            job_query, {
                              'Content-Type' => 'application/json; charset=UTF-8',
                              "authorization" => "Bearer " + token
                            })
     detail_hash = JSON.parse(info.content)["rows"][0]
     detail_parameters = Hash(title: detail_hash["Degree"],
-                             url: "https://jobboard.blueskymss.com/JobBoard/jobBoard.html?a=247medstaff&it=3&s=1&bss=2&c=1&fv=111&fc=1#",
+                             url: SITE_URL,
                              job_reference: parameters_row["Id"],
                              city: detail_hash["City"],
                              state: detail_hash["State"],
